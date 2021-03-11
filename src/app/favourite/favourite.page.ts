6bc1f5e8ba740bbc10f460f1d0caaaa137009226
CynthiaBudiono <c14180210@john.petra.ac.id>
2021-03-11 23:12:17 +0800

    rev fav

--- src/app/favourite/favourite.page.ts
+++ src/app/favourite/favourite.page.ts
@@ -33,7 +33,7 @@ export class FavouritePage implements OnInit {
 
   makefav(a) {
     // alert("aaaaa : "+a);
-    this.notesf[a][3] = 0;
+    this.notesf[a].setfav(0);
     this.globalvar.arrayn[a].setfav(0);
     this.notesf.splice(a,1);
     this.getindex.splice(a,1);
